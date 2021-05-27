68f8dd0cfeaa87a219377c4027ed329b98897b78
Annika Ö <annika.oesterdiekhoff@ewe.net>
2021-05-27 17:41:16 +0200

    add s-step extension

--- ISPAM_algorithm.py
+++ ISPAM_algorithm.py
@@ -67,6 +67,7 @@ def I_SPAM_algorithm(appear_dict: dict[Any, ndarray], min_sup, dataset_sequences
                 if value_app[-i] == 1:
                     bit_dict[i].pop(itemk)
             appear_dict_copy.pop(itemk)
+            fp_dict.pop(itemk)
         else:
             list_l.append(itemk)
     appear_dict = appear_dict_copy.copy()
@@ -81,23 +82,69 @@ def I_SPAM_algorithm(appear_dict: dict[Any, ndarray], min_sup, dataset_sequences
 
 
 def m_dfs(item_t, appear_dict, fp_dict, s_temp_t, i_temp_t, min_sup, bit_dict):
-    print("item_t", item_t)
+    print("item_t here", item_t)
     for s_extension_element in s_temp_t:
         # convert to int for bitwise and
         print("s_extension_element", s_extension_element)
         appear_t = appear_dict[item_t].astype(int)
         appear_s_extension_element = appear_dict[s_extension_element].astype(int)
         approximate_appear_t_s = appear_t & appear_s_extension_element
-        print(approximate_appear_t_s)
-        if one_count(appear_s_extension_element) >= min_sup:
+        print("approximate_appear_t_s", approximate_appear_t_s)
+
+        appear_t_s = np.zeros((len(approximate_appear_t_s)))
+        fp_t_s = np.zeros((len(approximate_appear_t_s)))
+
+        if one_count(approximate_appear_t_s) >= min_sup:
             for bit_k in range(len(approximate_appear_t_s)):
-                if approximate_appear_t_s[bit_k] != 0:
-                    if fp_dict[item_t][bit_k] >= fp_dict[s_extension_element][bit_k]:
-                        bit_s_extension_element = bit_dict[bit_k][s_extension_element]
+                print("bit_k", bit_k)
+                print("index von hinten", -(bit_k+1))
+                if approximate_appear_t_s[-(bit_k+1)] != 0:
+                    fp_item_t = int(fp_dict[item_t][-(bit_k+1)])
+                    fp_s = int(fp_dict[s_extension_element][-(bit_k+1)])
+                    print("fp_item_t", fp_item_t)
+                    print("fp_s", fp_s)
+                    if fp_item_t >= fp_s:
+                        print("bit_dict", bit_dict[bit_k+1])
+                        print("bit_k for dict", bit_k+1)
+                        print("s_extension_element", s_extension_element)
+                        bit_s_extension_element = bit_dict[bit_k+1][s_extension_element]
                         print("bit_s_extension_element", bit_s_extension_element)
+
+                        for shifting_number in range(fp_item_t):
+                            print("shifting_number", shifting_number+1)
+                            bit_s_extension_element = np.roll(bit_s_extension_element, 1)
+                            bit_s_extension_element[0] = 0
+                        print("bit_s_extension_element after shifting", bit_s_extension_element)
+
+                        if one_count(bit_s_extension_element) > 0:
+                            list_of_ones = np.where(bit_s_extension_element == 1)
+                            print("list_of_ones", list_of_ones)
+                            h = len(bit_s_extension_element) - np.max(list_of_ones)
+                            appear_t_s[-(bit_k+1)] = 1
+                            fp_t_s[-(bit_k+1)] = fp_item_t + h
+                        print("current appear_t_s", appear_t_s)
+                        print("current fp_t_s", fp_t_s)
+
+                    else:
+                        appear_t_s[-(bit_k + 1)] = 1
+                        fp_t_s[-(bit_k + 1)] = fp_s
+                        print("current appear_t_s", appear_t_s)
+                        print("current fp_t_s", fp_t_s)
                 else:
-                    appear_dict[item_t + s_extension_element] = 0
-                    fp_dict[item_t + s_extension_element] = 0
+                    print("or here")
+                    appear_t_s[-(bit_k + 1)] = 0
+                    fp_t_s[-(bit_k + 1)] = 0
+                    print("current appear_t_s", appear_t_s)
+                    print("current fp_t_s", fp_t_s)
+            print("appear_t_s", appear_t_s)
+            if one_count(appear_t_s) >= min_sup:
+                print("never goes here")
+                appear_dict[item_t + s_extension_element] = appear_t_s
+                fp_dict[item_t + s_extension_element] = fp_t_s
+                m_dfs(item_t + s_extension_element, appear_dict, fp_dict, s_temp_t, i_temp_t, min_sup, bit_dict)
+
+        print("appear_dict", appear_dict)
+        print("fp_dict", fp_dict)
 
 
 def one_count(array):
