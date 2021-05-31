4729f2c4234a67430c8312ccd671cf2d1c86bb31
Annika Ö <annika.oesterdiekhoff@ewe.net>
2021-05-31 18:16:42 +0200

    finish ispam

--- ISPAM_algorithm.py
+++ ISPAM_algorithm.py
@@ -1,9 +1,11 @@
 import numpy as np
 from numpy import ndarray
 from typing import Any
+from SPAM_algorithm import check_support
 
 
-def init_bit_appering_and_first_position_sequences(dataset_sequences: dict[int, list[list[str]]], list_of_words: list[str]) \
+def init_bit_appering_and_first_position_sequences(dataset_sequences: dict[int, list[list[str]]],
+                                                   list_of_words: list[str]) \
         -> tuple[dict[Any, ndarray], dict[Any, ndarray], dict[Any, dict[Any, ndarray]]]:
     """
 
@@ -18,13 +20,13 @@ def init_bit_appering_and_first_position_sequences(dataset_sequences: dict[int, 
         position_j = 1
         for sequence_j in value:
             for item_k in sequence_j:
-                if appear_dict[item_k][element_i] == 0:
-                    appear_dict[item_k][element_i] = 1
+                if appear_dict['[' + item_k + ']'][element_i] == 0:
+                    appear_dict['[' + item_k + ']'][element_i] = 1
 
-                bit_dict[key][item_k][-position_j] = 1
+                bit_dict[key]['[' + item_k + ']'][-position_j] = 1
 
-                if fp_dict[item_k][element_i] == 0:
-                    fp_dict[item_k][element_i] = position_j
+                if fp_dict['[' + item_k + ']'][element_i] == 0:
+                    fp_dict['[' + item_k + ']'][element_i] = position_j
             position_j += 1
         element_i -= 1
 
@@ -32,7 +34,7 @@ def init_bit_appering_and_first_position_sequences(dataset_sequences: dict[int, 
 
 
 def initialise_zero_sequences(dataset_sequences: dict[int, list[list[str]]], list_of_words: list[str]) \
-    -> tuple[dict[Any, ndarray], dict[Any, ndarray], dict[Any, dict[Any, ndarray]]]:
+        -> tuple[dict[Any, ndarray], dict[Any, ndarray], dict[Any, dict[Any, ndarray]]]:
     """
 
     :param dataset_sequences:
@@ -43,21 +45,20 @@ def initialise_zero_sequences(dataset_sequences: dict[int, list[list[str]]], lis
     fp_dict = dict()
     bit_dict = dict()
     for word in list_of_words:
-        appear_dict[word] = np.zeros(len(dataset_sequences))
-        fp_dict[word] = np.zeros(len(dataset_sequences))
+        appear_dict['[' + word + ']'] = np.zeros(len(dataset_sequences))
+        fp_dict['[' + word + ']'] = np.zeros(len(dataset_sequences))
     for key, value in dataset_sequences.items():
         dict_sid = dict()
         for sequence in value:
             for item in sequence:
                 if item not in dict_sid.keys():
-                    dict_sid[item] = np.zeros(len(value))
+                    dict_sid['[' + item + ']'] = np.zeros(len(value))
             bit_dict[key] = dict_sid
 
     return appear_dict, fp_dict, bit_dict
 
 
 def I_SPAM_algorithm(appear_dict: dict[Any, ndarray], min_sup, dataset_sequences, bit_dict, fp_dict):
-
     list_l = list()
     appear_dict_copy = appear_dict.copy()
     for itemk, value_app in appear_dict.items():
@@ -72,6 +73,11 @@ def I_SPAM_algorithm(appear_dict: dict[Any, ndarray], min_sup, dataset_sequences
             list_l.append(itemk)
     appear_dict = appear_dict_copy.copy()
 
+    dict_support = dict()
+    for key, value in appear_dict.items():
+        if check_support(value) >= min_sup:
+            dict_support[key] = check_support(value)
+
     i_temp_t_dict = dict()
     for item_t in list_l:
         i_temp_t = [x for x in list_l if x > item_t]
@@ -81,10 +87,11 @@ def I_SPAM_algorithm(appear_dict: dict[Any, ndarray], min_sup, dataset_sequences
         s_temp_t = list_l
         i_temp_t = [x for x in list_l if x > item_t]
         # Call M_DFS(T, AppearT, FPT, S-temp<T>, I-temp<T>)
-        m_dfs(item_t, appear_dict, fp_dict, s_temp_t, min_sup, bit_dict, i_temp_t_dict)
+        appear_dict, fp_dict, dict_support = m_dfs(item_t, appear_dict, fp_dict, s_temp_t, min_sup, bit_dict, i_temp_t_dict, dict_support)
+    return appear_dict, fp_dict, dict_support
 
 
-def m_dfs(item_t, appear_dict, fp_dict, s_temp_t, min_sup, bit_dict, i_temp_t_dict):
+def m_dfs(item_t, appear_dict, fp_dict, s_temp_t, min_sup, bit_dict, i_temp_t_dict, dict_support):
     appear_t = appear_dict[item_t].astype(int)
 
     # s-step
@@ -99,12 +106,12 @@ def m_dfs(item_t, appear_dict, fp_dict, s_temp_t, min_sup, bit_dict, i_temp_t_di
         if one_count(approximate_appear_t_s) >= min_sup:
             for bit_k in range(len(approximate_appear_t_s)):
 
-                if approximate_appear_t_s[-(bit_k+1)] != 0:
-                    fp_item_t = int(fp_dict[item_t][-(bit_k+1)])
-                    fp_s = int(fp_dict[s_extension_element][-(bit_k+1)])
+                if approximate_appear_t_s[-(bit_k + 1)] != 0:
+                    fp_item_t = int(fp_dict[item_t][-(bit_k + 1)])
+                    fp_s = int(fp_dict[s_extension_element][-(bit_k + 1)])
 
                     if fp_item_t >= fp_s:
-                        bit_s_extension_element = bit_dict[bit_k+1][s_extension_element]
+                        bit_s_extension_element = bit_dict[bit_k + 1][s_extension_element]
 
                         for shifting_number in range(fp_item_t):
                             bit_s_extension_element = np.roll(bit_s_extension_element, 1)
@@ -113,8 +120,8 @@ def m_dfs(item_t, appear_dict, fp_dict, s_temp_t, min_sup, bit_dict, i_temp_t_di
                         if one_count(bit_s_extension_element) > 0:
                             list_of_ones = np.where(bit_s_extension_element == 1)
                             h = len(bit_s_extension_element) - np.max(list_of_ones)
-                            appear_t_s[-(bit_k+1)] = 1
-                            fp_t_s[-(bit_k+1)] = fp_item_t + h
+                            appear_t_s[-(bit_k + 1)] = 1
+                            fp_t_s[-(bit_k + 1)] = fp_item_t + h
 
                     else:
                         appear_t_s[-(bit_k + 1)] = 1
@@ -126,14 +133,13 @@ def m_dfs(item_t, appear_dict, fp_dict, s_temp_t, min_sup, bit_dict, i_temp_t_di
             if one_count(appear_t_s) >= min_sup:
                 appear_dict[item_t + s_extension_element] = appear_t_s
                 fp_dict[item_t + s_extension_element] = fp_t_s
-                m_dfs(item_t + s_extension_element, appear_dict, fp_dict, s_temp_t, min_sup, bit_dict, i_temp_t_dict)
-
+                dict_support[item_t + s_extension_element] = check_support(appear_t_s)
+                m_dfs(item_t + s_extension_element, appear_dict, fp_dict, s_temp_t, min_sup, bit_dict, i_temp_t_dict, dict_support)
 
     # i-step
-    if item_t[-1] == ')':
-        i_temp_t = i_temp_t_dict[item_t[-2]]
-    else:
-        i_temp_t = i_temp_t_dict[item_t[-1]]
+    last_sequence = item_t.split("[")
+    splitted_item_t = last_sequence[-1].split(',')
+    i_temp_t = i_temp_t_dict['[' + splitted_item_t[-1]]
 
     for i_extension_element in i_temp_t:
         # convert to int for bitwise and
@@ -146,20 +152,20 @@ def m_dfs(item_t, appear_dict, fp_dict, s_temp_t, min_sup, bit_dict, i_temp_t_di
         if one_count(approximate_appear_t_i) >= min_sup:
             for bit_k in range(len(approximate_appear_t_i)):
 
-                if approximate_appear_t_i[-(bit_k+1)] != 0:
-                    fp_item_t = int(fp_dict[item_t][-(bit_k+1)])
-                    fp_i = int(fp_dict[i_extension_element][-(bit_k+1)])
+                if approximate_appear_t_i[-(bit_k + 1)] != 0:
+                    fp_item_t = int(fp_dict[item_t][-(bit_k + 1)])
+                    fp_i = int(fp_dict[i_extension_element][-(bit_k + 1)])
 
                     if fp_item_t == fp_i:
                         appear_t_i[-(bit_k + 1)] = 1
                         fp_t_i[-(bit_k + 1)] = fp_item_t
 
                     else:
-                        last_item = item_t[-1]
-                        if last_item == ')':
-                            last_item = item_t[-2]
-                        last_item_bit = bit_dict[bit_k+1][last_item].astype(int)
-                        i_extension_element_bit = bit_dict[bit_k+1][i_extension_element].astype(int)
+                        last_sequence_1 = item_t.split("[")
+                        splitted_item_t_1 = last_sequence_1[-1].split(',')
+                        last_item = splitted_item_t_1[-1][:-1]
+                        last_item_bit = bit_dict[bit_k + 1]['[' + last_item + ']'].astype(int)
+                        i_extension_element_bit = bit_dict[bit_k + 1][i_extension_element].astype(int)
                         and_bit = last_item_bit & i_extension_element_bit
 
                         if one_count(and_bit) > 0:
@@ -179,22 +185,15 @@ def m_dfs(item_t, appear_dict, fp_dict, s_temp_t, min_sup, bit_dict, i_temp_t_di
                     fp_t_i[-(bit_k + 1)] = 0
 
             if one_count(appear_t_i) >= min_sup:
-                if item_t[-1] == ')':
-                    str_index = item_t[:-1] + i_extension_element + ')'
-                else:
-                    str_index = item_t[:-1] + '(' + item_t[-1] + i_extension_element + ')'
+                str_index = item_t[:-1] + ',' + i_extension_element[1:]
                 appear_dict[str_index] = appear_t_i
                 fp_dict[str_index] = fp_t_i
-                m_dfs(str_index, appear_dict, fp_dict, s_temp_t, min_sup, bit_dict, i_temp_t_dict)
+                dict_support[str_index] = check_support(appear_t_i)
+                m_dfs(str_index, appear_dict, fp_dict, s_temp_t, min_sup, bit_dict, i_temp_t_dict, dict_support)
 
-    print("appear_dict", appear_dict)
-    print("fp_dict", fp_dict)
+    return appear_dict, fp_dict, dict_support
 
 
 def one_count(array):
     array_ones = np.where(array == 1)
     return np.size(array_ones)
-
-
-def s_step():
-    pass
--- experiments_tweets.py
+++ experiments_tweets.py
@@ -1,5 +1,6 @@
-from SPAM_algorithm import bitmap_representation, create_tree, get_frequent_sequences, plot_frequent_sequence, create_wordcloud
+from SPAM_algorithm import bitmap_representation, create_tree, get_frequent_sequences, plot_frequent_sequence, create_wordcloud, dataset_transformation
 from twitter import read_twitter_csv_into_dataset, sort_and_anonymize_dataset
+from ISPAM_algorithm import init_bit_appering_and_first_position_sequences, I_SPAM_algorithm
 
 
 def run_experiment(sorted_dataset_Cid_Tid, word_list, minSup, max_number_sequence=2):
@@ -22,27 +23,44 @@ def run_experiment(sorted_dataset_Cid_Tid, word_list, minSup, max_number_sequenc
                                             limit=max_number_sequence)
 
     frequent_sequences = get_frequent_sequences(dict_bitmap)
-    print(frequent_sequences)
-    print(dict_support)
+    print("spam", frequent_sequences)
+    print("spam", dict_support)
 
-    plot_frequent_sequence(dict_support)
-    create_wordcloud(dict_support)
+    # plot_frequent_sequence(dict_support)
+    # create_wordcloud(dict_support)
+
+
+def run_experiment_ispam(dataset_Cid_Tid, list_of_words, minSup):
+
+    dataset_sequences = dataset_transformation(dataset_Cid_Tid)
+    appear_dict, fp_dict, bit_dict = init_bit_appering_and_first_position_sequences(dataset_sequences, list_of_words)
+
+    appear_dict, fp_dict, dict_support = I_SPAM_algorithm(appear_dict, minSup, dataset_sequences, bit_dict, fp_dict)
+    frequent_sequences = get_frequent_sequences(appear_dict)
+    print("ispam", frequent_sequences)
+    print("ispam", dict_support)
+
+    # plot_frequent_sequence(dict_support)
+    # create_wordcloud(dict_support)
 
 
 if __name__ == "__main__":
     # nltk.download('stopwords')
 
-    # ### experiment 2
-    # # try SPAM algorithm with first 100 tweets and minimum support of 5
-    # dataset_Cid_Tid_100, word_list_100, dict_usernames_100 = read_twitter_csv_into_dataset(number_of_tweets=100)
-    # # sorting
-    # sorted_dataset_Cid_Tid_100 = sort_and_anonymize_dataset(dataset_Cid_Tid_100, dict_usernames_100)
-    # word_list_100 = sorted(word_list_100)
-    #
-    # minSup_2 = 5
-    # # here is 2 enough because in data set there are not more than two transactions per tweet user
-    # max_number_sequence_2 = 2
-    # run_experiment(sorted_dataset_Cid_Tid_100, word_list_100, minSup_2, max_number_sequence_2)
+    ### experiment 2
+    # try SPAM algorithm with first 100 tweets and minimum support of 5
+    dataset_Cid_Tid_100, word_list_100, dict_usernames_100 = read_twitter_csv_into_dataset(number_of_tweets=100)
+    # sorting
+    sorted_dataset_Cid_Tid_100 = sort_and_anonymize_dataset(dataset_Cid_Tid_100, dict_usernames_100, customer_starting_index=1)
+    word_list_100 = sorted(word_list_100)
+
+    minSup_2 = 5
+    # here is 2 enough because in data set there are not more than two transactions per tweet user
+    max_number_sequence_2 = 2
+    # SPAM
+    run_experiment(sorted_dataset_Cid_Tid_100, word_list_100, minSup_2, max_number_sequence_2)
+    # ISPAM
+    run_experiment_ispam(sorted_dataset_Cid_Tid_100, word_list_100, minSup_2)
 
     ### experiment 3: DOESN'T WORK
     # try SPAM algorithm with all tweets and minSup of 50
@@ -59,10 +77,16 @@ if __name__ == "__main__":
     minSup_4 = 50
     # one person with 27 tweets, next most tweets from one person are 9
     max_number_sequence_4 = 9
-    # run_experiment(sorted_dataset_Cid_Tid_1000, word_list_1000, minSup_4, max_number_sequence_4)
+    # SPAM
+    run_experiment(sorted_dataset_Cid_Tid_1000, word_list_1000, minSup_4, max_number_sequence_4)
+    # ISPAM
+    run_experiment_ispam(sorted_dataset_Cid_Tid_1000, word_list_1000, minSup_4)
 
     ### experiment 5:
     # try SPAM algorithm with first 1000 tweets and minimum support of 30
 
     minSup_5 = 30
+    # SPAM
     run_experiment(sorted_dataset_Cid_Tid_1000, word_list_1000, minSup_5, max_number_sequence_4)
+    # ISPAM
+    run_experiment_ispam(sorted_dataset_Cid_Tid_1000, word_list_1000, minSup_5)
